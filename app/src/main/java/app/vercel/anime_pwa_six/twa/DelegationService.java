7d42f75389c8f1f5a10e16e31c45711ca2f43a52
Vatsal Budhbhatti <vatsalbudh@gmail.com>
2025-11-03 01:26:24 +0530

    Created app with TWA

---
+++ app/src/main/java/app/vercel/anime_pwa_six/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.anime_pwa_six.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
